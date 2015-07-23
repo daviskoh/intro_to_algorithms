303314c69715d0f6c27ad63806ca6cd48b1a290e
daviskoh <koh.davis.0@gmail.com>
2015-07-22 23:18:28 -0400

    init

---
+++ lectures/3/StrassensSubcubicMatrixMultiplication/StrassensSubcubicMatrixMultiplication/StrassensSubcubicMatrixMultiplication.h
@@ -0,0 +1,19 @@
+//
+//  StrassensSubcubicMatrixMultiplication.h
+//  StrassensSubcubicMatrixMultiplication
+//
+//  Created by Davis Koh on 7/22/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StrassensSubcubicMatrixMultiplication.
+FOUNDATION_EXPORT double StrassensSubcubicMatrixMultiplicationVersionNumber;
+
+//! Project version string for StrassensSubcubicMatrixMultiplication.
+FOUNDATION_EXPORT const unsigned char StrassensSubcubicMatrixMultiplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StrassensSubcubicMatrixMultiplication/PublicHeader.h>
+
+
